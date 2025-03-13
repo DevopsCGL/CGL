4f7d84a6d0c019f84d1b3556f7a4291a2d2c1e6d
Lukerioux <luluke44@gmail.com>
2025-03-13 11:06:15 +0100

    Update CourseList.js

--- front/src/components/CourseList.js
+++ front/src/components/CourseList.js
@@ -1,15 +1,1 @@
-import React from 'react';
 
-function CourseList({ courses }) {
-  return (
-    <div>
-      <ul>
-        {courses.map((course) => (
-          <li key={course.id}>{course.item}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-export default CourseList;
